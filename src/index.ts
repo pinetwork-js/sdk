a6574b1d7e223cbff1b1d3184f59a1627d3abe7b
Brewal Derouet <brewal_derouet@hotmail.fr>
2023-03-04 12:04:24 +0100

    feat: add Pi client to window object

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,13 @@ import { PiClient } from './pi-client';
 
 export const Pi = new PiClient();
 
+declare global {
+    interface Window {
+      Pi: PiClient;
+    }
+}
+
+window.Pi = Pi;
+
 export * from './handlers';
 export * from './pi-client';
